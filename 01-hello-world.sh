a321c47d56301446e5803e2d25ffd6677a1cd651
Hariram2023 <harivfx7@gmail.com>
2023-09-15 14:35:31 +0100

    hello world

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "hi heloo world ,i am learning devops with aws"
-PERSON1=Ramesh
-PERSON2=Suresh
+PERSON1=Sachin
+PERSON2=Rohit
 
 echo "$PERSON1: hi $PERSON2,good morning"
 echo "$PERSON2: good morning $PERSON1"
 echo "$PERSON1: how are u $PERSON2"
-echo "$PERSON2: i an good,how are you $PERSON1"
+echo "$PERSON2: i am good,how are you $PERSON1"

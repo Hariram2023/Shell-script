b788c405c6093a8372e0e350ce20acc4641907ac
Hariram2023 <harivfx7@gmail.com>
2023-09-15 13:43:59 +0100

    ramesh and suresh

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "hi heloo world ,i am learning devops with aws"
+
+echo "Ramesh: hi Suresh,good morning"
+echo "Suresh: good morning Ramesh"
+echo "Ramesh: how are u Suresh"
+echo "Suresh: i an good,how are you Ramesh"

7a3b1c61f3bf72c8ca3bb2375af9bdc1d80ca2b5
Hariram2023 <harivfx7@gmail.com>
2023-09-26 19:00:09 +0100

    added conditions

---
+++ 07-conditions.sh
@@ -0,0 +1,11 @@
+#!/bi /bash
+
+#check if the number is greater than 10 or not
+NUMBER=$1
+
+if [ $NUMBER -gt 10 ]
+then 
+    echo "$NUMBER is grater than 10"
+else 
+    echo "$NUMBER is not garter than 10"
+fi   
